f78f616dbf9d03f06b8d4090abc31f69a1a57023
yahya <yahya.comr565@gmail.com>
2019-05-21 05:08:51 +0300

    ecommerce-laravel5.6

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Admin;
+
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function login(Request $request){
+
+        if($request->isMethod('post')){
+    		$data = $request->input();
+            $adminCount = Admin::where(['email' => $data['email'],'password'=>md5($data['password']),'admin'=>1])->count(); 
+            if($adminCount > 0){
+                //echo "Success"; die;
+                Session::put('adminSession', $data['username']);
+                return redirect('/admin/dashboard');
+        	}else{
+                //echo "failed"; die;
+                return redirect('/admin')->with('flash_message_error','Invalid Username or Password');
+        	}
+    	}
+    	return view('admin.admin_login');
+    }
+}
